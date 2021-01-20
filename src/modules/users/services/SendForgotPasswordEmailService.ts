289f46be76b23f1d4388544a3736806d4bd78889
João Pedro Israel <juanpeterisrael@gmail.com>
2021-01-20 12:22:47 -0300

    Gera emails normalmente

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,7 +4,6 @@ import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
-// import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   email: string;
@@ -29,11 +28,22 @@ class SendForgotPasswordEmailService {
     if (!user) {
       throw new AppError('User does not exist.');
     }
-    await this.userTokensRepository.generate(user.id);
-    await this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido',
-    );
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
