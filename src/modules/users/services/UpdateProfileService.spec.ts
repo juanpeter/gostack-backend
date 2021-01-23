6be2965d8c05ad864b4b97740fffe5ba925f9ef9
João Pedro Israel <juanpeterisrael@gmail.com>
2021-01-23 13:41:03 -0300

    Perfil de usuário completo

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -33,6 +33,15 @@ describe('UpdateProfile', () => {
     expect(updatedUser.name).toBe('John Trê');
     expect(updatedUser.email).toBe('johntre@example.com');
   });
+  it('should not be able to update the profile from non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    );
+  });
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
